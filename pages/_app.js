8b6dfc6c279ee1638625fb8f522468411a9bce5f
nbc7 <90021751+nbc7@users.noreply.github.com>
2022-10-02 17:08:57 -0300

    initial structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Rockr Blog</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
